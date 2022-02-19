e21018519eec6c6f004fd70f42f6722d626dbb33
Alex-DiDo <91150616+Alex-DiDo@users.noreply.github.com>
2022-02-18 23:44:05 -0500

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,12 @@
+'''
+#CS175L-50
+#Name: Alexander DiDomenico
+#myInfo program
+'''
+#Name
+name = print("My name is Alexander DiDomenico")
+#Adress
+adress = print("My adress is 3914 Bergen Tpke, North Bergen, NJ, 07047")
+#Major
+major = print("My college major is Computer Science")
+print("Goodbye!")
